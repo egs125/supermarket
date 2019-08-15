c3af1edf13cf2ce6160d4280d37cb1dab41a96f7
egs125 <egs125@gmail.com>
2019-08-15 19:31:06 +0900

    add total

--- src/components/SuperMarket.js
+++ src/components/SuperMarket.js
@@ -2,10 +2,11 @@ import React from 'react';
 import SuperMarketTemplate from './SuperMarketTemplate';
 import ShopItemList from './ShopItemList';
 import BasketItemList from './BasketItemList';
+import TotalPrice from './TotalPrice';
 
 const SuperMarket = () => {
   return (
-    <SuperMarketTemplate items={<ShopItemList />} basket={<BasketItemList />} />
+  <SuperMarketTemplate items={<ShopItemList />} basket={<BasketItemList />} total={<TotalPrice />} />
   );
 };
 
